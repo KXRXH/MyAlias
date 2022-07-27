910e2a70b47974002bb3ab42d2fd987d6ae1ae27
KXRXH <kirayfolon@gmail.com>
2022-07-27 09:04:45 +0700

    ..

--- GolangServer/api/handlers.go
+++ GolangServer/api/handlers.go
@@ -1,14 +1,57 @@
 package api
 
 import (
+	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/gofiber/fiber/v2"
-
+	"github.com/kxrxh/alias-backend/game"
+	"github.com/kxrxh/alias-backend/models"
+	"github.com/kxrxh/alias-backend/utils"
 )
 
+// Ping/pong request
 func PingHandler(context *fiber.Ctx) error {
 	return context.Status(http.StatusOK).JSON(&fiber.Map{
 		"message": "pong",
+	})
+}
+
+func ConnectHandler(context *fiber.Ctx) error {
+	context.Accepts("application/json") // "application/json"
+	id, err := strconv.Atoi(context.Params("room_id"))
+	if err != nil {
+		return context.Status(http.StatusBadRequest).JSON(&fiber.Map{
+			"message": "Error has occurred",
+			"error":   err.Error(),
+		})
+	}
+	model := models.User{}
+	if err := context.BodyParser(&model); err != nil {
+		return context.Status(http.StatusBadRequest).JSON(&fiber.Map{
+			"message": "Error has occurred",
+			"error":   err.Error(),
+		})
+	}
+	game.ConnectUserToRoom(id, model)
+	return context.Status(http.StatusOK).JSON(&fiber.Map{
+		"message": fmt.Sprintf("User %s has been connected to room %v\n", model.NickName, id),
+		"room":    game.GetRoomById(id),
+	})
+}
+
+func DisconnectHandler(context *fiber.Ctx) error {
+	context.Accepts("application/json") // "application/json"
+	return nil
+}
+
+func NewRoomHandler(context *fiber.Ctx) error {
+	context.Accepts("application/json") // "application/json"
+	model := models.Room{RoomId: utils.GenerateRandomId(), UserList: []models.User{}, State: models.State{StateName: "wait", TimeLeft: 60}, Teams: []int{}}
+	game.CreateNewRoom(model)
+	return context.Status(http.StatusOK).JSON(&fiber.Map{
+		"message": "Room was created successfully",
+		"rooms":   game.GetAllRooms(),
 	})
 }
--- GolangServer/main.go
+++ GolangServer/main.go
@@ -4,14 +4,19 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/kxrxh/alias-backend/api"
-
 )
 
 func main() {
+
 	app := fiber.New()
+	// Enable CORS.
 	app.Use(cors.New(cors.Config{
 		AllowHeaders: "Origin, Content-Type, Accept",
 	}))
+	// Routes for accessing to server
 	app.Get("/ping", api.PingHandler)
+	app.Post("/user/connect/:room_id", api.ConnectHandler)
+	app.Post("/user/disconnect/:room_id", api.DisconnectHandler)
+	app.Post("/room/new", api.NewRoomHandler)
 	app.Listen("localhost:8080")
 }
